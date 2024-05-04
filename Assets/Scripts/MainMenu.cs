e758eb34e805979a110699f4f6967f1b94bb9101
Juno-Cheng <chengjonathan2021@gmail.com>
2024-05-04 15:47:05 -0700

    Mic Check

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject micCheckmark;
+    public GameObject micCross;
+
     void Start()
     {
-        
+        // Check if microphone is available
+        if (Microphone.devices.Length > 0)
+        {
+            Debug.Log("Microphone is available.");
+            // Enable the mic checkmark UI
+            micCheckmark.SetActive(true);
+            // Disable the mic cross UI
+            micCross.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No microphone detected.");
+            // Enable the mic cross UI
+            micCross.SetActive(true);
+            // Disable the mic checkmark UI
+            micCheckmark.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
